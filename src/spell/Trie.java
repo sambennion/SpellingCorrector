4f6749fd9d2da38a9ae61df7ed3545b4657ce479
sambennion <bennionsamuel@gmail.com>
2021-09-11 15:09:30 -0600

    Initialize Spelling Collector

---
+++ src/spell/Trie.java
@@ -0,0 +1,24 @@
+package spell;
+
+public class Trie implements ITrie{
+
+    @Override
+    public void add(String word) {
+
+    }
+
+    @Override
+    public INode find(String word) {
+        return null;
+    }
+
+    @Override
+    public int getWordCount() {
+        return 0;
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+}
